ecfda90a412ff99a1d0b2d6f2349e1890b52c394
Benzoh.Wada <benzoh.wada@gmail.com>
2016-02-25 23:17:47 +0900

    START

---
+++ study.swift
@@ -0,0 +1,19 @@
+//
+//  study.swift
+//  
+//
+//  Created by 和田充弘 on 2016/02/25.
+//
+//
+
+import Foundation
+
+// 2016/02/25
+print("hello, world!!")
+
+// これをターミナルで実行してみる
+
+// ---
+// Benzoh-MBP:MyStudy Benzoh$ swift study.swift
+// hello, world!!
+// ---
